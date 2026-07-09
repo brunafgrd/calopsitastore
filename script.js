4a94f08047c05d13915b7cab79facb7b13c9b3bc
Bruna Figueiredo <7v8sz6j454@privaterelay.appleid.com>
2026-07-09 20:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const imagens = [
+    "Calopsita1.avif",
+    "Calopsita2.avif",
+    "Calopsita3.avif"
+];
+
+let atual = 0;
+
+const banner = document.getElementById("info-banner");
+const dots = document.querySelectorAll(".dot");
+
+function atualizarBanner() {
+
+    banner.src = imagens[atual];
+
+    dots.forEach(dot => {
+        dot.classList.remove("active");
+    });
+
+    dots[atual].classList.add("active");
+
+}
+
+document.querySelector(".next").addEventListener("click", function () {
+
+    atual++;
+
+    if (atual === imagens.length) {
+        atual = 0;
+    }
+
+    atualizarBanner();
+
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+
+    atual--;
+
+    if (atual < 0) {
+        atual = imagens.length - 1;
+    }
+
+    atualizarBanner();
+
+});
+
+
